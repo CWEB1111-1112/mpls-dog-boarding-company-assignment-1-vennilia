955a9cb17ca401fc1ce1e0b33232d87506e0ee13
vennillia <blackkitten0315@gmail.com>
2021-02-04 13:31:16 -0600

    initial commit

---
+++ program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace assignment1
+{
+    /**
+        Name: Kei 
+        Date: 2/4/21
+    **/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
